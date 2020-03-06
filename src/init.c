5f9ad173ce324b2bc69212ad0ba469f3cdb7dc8b
Mark Dastmalchi-Round <github@markround.com>
2020-03-06 13:55:38 +0000

    Prevent init being run twice

--- src/init.c
+++ src/init.c
@@ -30,15 +30,22 @@ int init(int opt)
     IDOS->Printf("Obtained lock on " SETCMD_PATH "\n");
   }
 
+  /* If SetCMD has already been initialised, then doing this again in a new CLI process
+   * will result in a DSI when the new CLI exits. I'm pretty sure this has to do with the
+   * pathlist not being freed; I still have to work out the best way to deal with this, for now
+   * we simply do not allow "setcmd init" to be run twice. 
+   */
   check_dup = (struct PathNode *)IDOS->RemoveCmdPathNode(pathlist, lock);
   if (check_dup) {
+    IDOS->Printf("ERROR: SetCmd already initialised.\n");
+    IDOS->UnLock(lock);
     if (DEBUG) {
-      IDOS->Printf("Found and removed existing SetCmd paths\n");
       dump_path_node(check_dup);
     }
-    pathlist=check_dup;
+    return RETURN_FAIL;
   }
 
+  // Set up the new pathlist with us at the start
   new_path = IDOS->AddCmdPathNode(pathlist, lock, ADDCMDPATHNODE_HEAD);
   if (DEBUG) {
     IDOS->Printf("---> After\n");
@@ -56,10 +63,7 @@ int init(int opt)
     // This shouldn't fail!
     IDOS->Printf("ERROR: Failed to set the new command path!\n");
     IDOS->Printf("This should never happen :(\n");
-    if (DEBUG) {
-      // %m and %n magic modifiers only available in kickstart 51.59
-      IDOS->Printf("DOS error message = %m, error code = %n\n",0);
-    }
+    debug_dos();
     return RETURN_FAIL;
   }
   
