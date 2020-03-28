ae535b184aa2c0eb17c00d4ed7d2ef5a8c33817a
Mark Dastmalchi-Round <github@markround.com>
2020-03-28 18:34:38 +0000

    more formatting

--- src/usage.c
+++ src/usage.c
@@ -8,37 +8,37 @@ void usage()
   IDOS->Printf("\n");
   IDOS->Printf("Usage:\n");
   IDOS->Printf("\n");
-  IDOS->Printf(BOLD_ON "init " BOLD_OFF FG_WHITE "quiet | verbose" FG_BLACK "\n");
+  IDOS->Printf("%sinit%s %squiet | verbose%s\n", fmt(BOLD_ON), fmt(BOLD_OFF), fmt(FG_WHITE), fmt(NORMAL));
   IDOS->Printf("  Sets up the SetCmd path for the current shell.\n");
   IDOS->Printf("  example: setcmd init verbose\n");
 
-  IDOS->Printf(BOLD_ON "list " BOLD_OFF FG_WHITE "verbose" FG_BLACK "\n");
-  IDOS->Printf("  List all commands managed by setcmd. The optional " FG_WHITE "verbose " FG_BLACK "argument shows additional detail.\n");
+  IDOS->Printf("%slist%s %sverbose%s\n", fmt(BOLD_ON), fmt(BOLD_OFF), fmt(FG_WHITE), fmt(NORMAL));
+  IDOS->Printf("  List all commands managed by setcmd. The optional %sverbose%s argument shows additional detail.\n", fmt(FG_WHITE), fmt(NORMAL));
   IDOS->Printf("  example: setcmd list\n");
 
 
-  IDOS->Printf(BOLD_ON "show " BOLD_OFF FG_BLUE "cmd" FG_BLACK "\n");
-  IDOS->Printf("  Shows details for specified command " FG_BLUE "cmd" FG_BLACK "\n");
+  IDOS->Printf("%sshow%s %scmd%s\n", fmt(BOLD_ON), fmt(BOLD_OFF), fmt(FG_BLUE), fmt(NORMAL));
+  IDOS->Printf("  Shows details for specified command %scmd%s\n", fmt(FG_BLUE), fmt(NORMAL));
   IDOS->Printf("  example: setcmd show uae\n");
 
-  IDOS->Printf(BOLD_ON "set-version " BOLD_OFF FG_BLUE "cmd version" FG_BLACK "\n");
-  IDOS->Printf("  Sets " FG_BLUE "cmd " FG_BLACK "to " FG_BLUE "version" FG_BLACK "\n");
+  IDOS->Printf("%sset-version%s %scmd version%s\n", fmt(BOLD_ON), fmt(BOLD_OFF), fmt(FG_BLUE), fmt(NORMAL));
+  IDOS->Printf("  Sets %scmd%s to %sversion%s\n", fmt(FG_BLUE), fmt(FG_BLACK), fmt(FG_BLUE), fmt(FG_BLACK));
   IDOS->Printf("  example: setcmd set-version uae system\n");
 
-  IDOS->Printf(BOLD_ON "add-cmd " BOLD_OFF FG_BLUE "cmd" FG_BLACK "\n");
-  IDOS->Printf("  Adds a new command " FG_BLUE "cmd " FG_BLACK "and points it to a stub\n");
+  IDOS->Printf("%sadd-cmd%s %scmd%s\n", fmt(BOLD_ON), fmt(BOLD_OFF), fmt(FG_BLUE), fmt(NORMAL));
+  IDOS->Printf("  Adds a new command %scmd%s and points it to a stub\n", fmt(FG_BLUE), fmt(NORMAL));
   IDOS->Printf("  example: setcmd add-cmd uae\n");
 
-  IDOS->Printf(BOLD_ON "delete-cmd " BOLD_OFF FG_BLUE "cmd" FG_BLACK "\n");
-  IDOS->Printf("  Removes command " FG_BLUE "cmd " FG_BLACK "from setcmd\n");
+  IDOS->Printf("%sdelete-cmd%s %scmd%s\n", fmt(BOLD_ON), fmt(BOLD_OFF), fmt(FG_BLUE), fmt(NORMAL));
+  IDOS->Printf("  Removes command %scmd%s from setcmd\n", fmt(FG_BLUE), fmt(NORMAL));
   IDOS->Printf("  example: setcmd delete-cmd uae\n");
 
-  IDOS->Printf(BOLD_ON "add-version " BOLD_OFF FG_BLUE "cmd version target" FG_BLACK "\n");
-  IDOS->Printf("  Adds a new version for " FG_BLUE "cmd" FG_BLACK ", named " FG_BLUE "version " FG_BLACK "and pointing at the path " FG_BLUE "target" FG_BLACK ".\n");
+  IDOS->Printf("%sadd-version%s %scmd version target%s\n", fmt(BOLD_ON), fmt(BOLD_OFF), fmt(FG_BLUE), fmt(NORMAL));
+  IDOS->Printf("  Adds a new version for %scmd%s, named %sversion%s and pointing at the path %starget%s\n", fmt(FG_BLUE), fmt(NORMAL), fmt(FG_BLUE), fmt(NORMAL), fmt(FG_BLUE), fmt(NORMAL));
   IDOS->Printf("  example: setcmd add-version uae system System:Emulation/E-UAE-amigfx/uae\n");
 
-  IDOS->Printf(BOLD_ON "delete-version " BOLD_OFF FG_BLUE "cmd version" FG_BLACK "\n");
-  IDOS->Printf("  Removes version " FG_BLUE "version " FG_BLACK "from " FG_BLUE "cmd" FG_BLACK "\n");
+  IDOS->Printf("%sdelete-version%s %scmd version%s\n", fmt(BOLD_ON), fmt(BOLD_OFF), fmt(FG_BLUE), fmt(NORMAL));
+  IDOS->Printf("  Removes version %sversion%s from %scmd%s\n", fmt(FG_BLUE), fmt(NORMAL), fmt(FG_BLUE), fmt(NORMAL));
   IDOS->Printf("  example: setcmd delete-version uae 1.0.0-beta\n");
   return;
 }
