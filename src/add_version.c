995da73d4a85c90249ad0789025a3d32b5ef1e08
Mark Dastmalchi-Round <github@markround.com>
2020-03-28 14:32:53 +0000

    started changing formatting

--- src/add_version.c
+++ src/add_version.c
@@ -17,7 +17,7 @@ int add_version(const char *cmd, const char *version, const char *target)
   strcpy(cmd_dir, SETCMD_CMDS);
   IDOS->AddPart(cmd_dir, cmd, MAX_PATH_BUF);
   if (!can_lock(cmd_dir)) {
-    IDOS->Printf(FG_RED "ERROR" NORMAL ": Command %s does not exist.\n", cmd);
+    IDOS->Printf("%s ERROR %s: Command %s does not exist.\n", fmt(FG_RED), fmt(NORMAL), cmd);
     dos_debug();  
     return RETURN_FAIL;
   }
@@ -26,7 +26,7 @@ int add_version(const char *cmd, const char *version, const char *target)
   strcpy(path, cmd_dir);
   IDOS->AddPart(path, version, MAX_PATH_BUF);
   if (can_lock(path)) {
-    IDOS->Printf(FG_RED "ERROR" NORMAL ": version %s already exists for command %s.\n", version, cmd);
+    IDOS->Printf("%s ERROR %s: version %s already exists for command %s.\n", fmt(FG_RED), fmt(NORMAL), version, cmd);
     dos_debug();
     return RETURN_FAIL;  
   }
@@ -34,7 +34,7 @@ int add_version(const char *cmd, const char *version, const char *target)
   // Create the link
   rc = IDOS->MakeLink((char *)path, (char *)target, LINK_SOFT);
   if (!rc) {
-    IDOS->Printf(FG_RED "ERROR" NORMAL ": unexpected error creating link %s => %s.\n", path, target);  
+    IDOS->Printf("%s ERROR %s: unexpected error creating link %s => %s.\n", fmt(FG_RED), fmt(NORMAL), path, target);  
     dos_debug();
   }
 
