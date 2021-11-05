7ae18af9351cfb3586e36a815e8ba562a9b71241
Mark Dastmalchi-Round <github@markround.com>
2021-11-05 10:51:17 +0000

    Start on verbose listing

--- src.68k/list.c
+++ src.68k/list.c
@@ -9,12 +9,13 @@
 int list(int opt) 
 {
   char cmd[MAX_PATH_BUF];
+  char cmd_dir[MAX_PATH_BUF];
   char link[MAX_PATH_BUF];
   char version[MAX_PATH_BUF];
   char current_version[MAX_PATH_BUF];
   char cmd_path[MAX_PATH_BUF];
   char target[MAX_PATH_BUF];
-  struct FileInfoBlock path_data;
+  struct FileInfoBlock path_data, cmd_data;
   struct DevProc *proc;
   BPTR path_lock, cmd_lock;
   int rc = 0;
@@ -29,16 +30,12 @@ int list(int opt)
     return RETURN_FAIL;
   }
 
-  // Now we need to get our MsgPort so we can use ReadLink later
-  //	GetDeviceProc -- Finds a handler to send a message to (V36), improved over DeviceProc
+  // Now we need to get our proc struct with MsgPort so we can use ReadLink later
   proc = NULL;
   proc = GetDeviceProc(SETCMD_PATH, proc);
   // Todo: Error handling
   if (proc != NULL) {
   }
-
-
-  printf("In Listing module\n");
 
   // Iterate over all the cmds. First get the data for the path dir
   rc = Examine(path_lock, &path_data);
@@ -50,45 +47,65 @@ int list(int opt)
 
   // Now loop over all directory entries
   while (ExNext(path_lock, &path_data)) {
-    /* path_data.fib_FileName has the filename so if we join it to SETCMD_PATH
-     * we can then lock and examine the individual commands / symlinks
-     */
 
-    // Extract the cmd name
+    // Extract the cmd name from path_data struct
     strcpy (cmd, path_data.fib_FileName);
-    if (DEBUG) {
-      printf("Examining command %s\n", cmd);
-    }
-
-    // Lock the command
+    
+    // Get the full path to the command
     strcpy(cmd_path, SETCMD_CMDS);
     AddPart(cmd_path, cmd, MAX_PATH_BUF);
-    cmd_lock = Lock(cmd_path, ACCESS_READ);
-    if (!cmd_lock) {
-      printf("%sERROR %s: Failed to lock the %s command\n", fmt(FG_RED), fmt(NORMAL), cmd_path);
-      printf("Check your installation and make sure the SETCMD: assign is correctly setup.\n");
-      printf("For more information see the SetCmd manual.\n");
-      cmd_rc = RETURN_FAIL;
-      goto cleanup;
-    }
-  
+      
     // Only process if it is a symlink
     if (ReadLink(proc->dvp_Port, path_lock, cmd, link, MAX_PATH_BUF)) {
       if (DEBUG) {
         printf("Following symlink for %s to %s\n", cmd_path, link);
-        strcpy (current_version, FilePart(link));
-        // skip if for whatever reason we don't have a link 
-        if (strlen(link) > 0) {
-          printf("%s [%s%s%s]\n", cmd, fmt(SELECTED), current_version, fmt(NORMAL));
+      }
+      
+      strcpy (current_version, FilePart(link));
+      // skip if for whatever reason we don't have a link 
+      if (strlen(link) > 0) {
+        // Basic listing
+        printf("%s [%s%s%s]\n", cmd, fmt(SELECTED), current_version, fmt(NORMAL));
+
+        if (opt == OPT_VERBOSE) {
+          /* Verbose mode - for every link, get a list of the versions under the
+           * relevant "cmd" directory, and highlight it if it's also the currently
+           * selected version.
+           */
+
+          // Full path to the relevant cmd dir
+          strcpy(cmd_dir, SETCMD_CMDS);
+          AddPart(cmd_dir, cmd, MAX_PATH_BUF);
+
+          // Now, get a directory listing of the available versions underneath this cmd directory
+          cmd_lock = Lock(cmd_dir, ACCESS_READ);
+          if (!cmd_lock) {
+            printf("%sERROR %s: Failed to lock the %s directory\n", fmt(FG_RED), fmt(NORMAL), cmd_dir);
+            printf("Check your installation and make sure the SETCMD: assign is correctly setup.\n");
+            printf("For more information see the SetCmd manual.\n");
+            cmd_rc = RETURN_FAIL;
+            goto cleanup;
+          }
+
+          rc = Examine(cmd_lock, &cmd_data);
+          if (!rc) {
+            printf("Failed to examine\n");
+            cmd_rc = RETURN_FAIL;
+            goto cleanup;
+          }
+
+          while (ExNext(cmd_lock, &cmd_data)) {
+            
+          } // End of looping over available versions
+
+          if (cmd_lock)  { UnLock(cmd_lock); }
         }
       }
     } else if (DEBUG) {
       printf("ReadLink for command %s failed\n", cmd); 
     }
 
-    if (cmd_lock) { UnLock(cmd_lock); }
-
-  }
+  } // End of looping over cmds
 
 cleanup:
   if (path_lock)  { UnLock(path_lock); }
